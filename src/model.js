a47bd2a8b9d1a876ee8d8be01d6003992b83ce53
BABAK0T0 <nrandria@student.42.fr>
2021-03-28 18:36:38 +0200

    Add Particles, set a new BufferGeometry

--- src/model.js
+++ src/model.js
@@ -1,10 +1,10 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { MeshSurfaceSampler } from "three/examples/jsm/math/MeshSurfaceSampler";
 
 export default class Model {
   constructor(obj) {
-    console.log(obj);
     this.name = obj.name;
     this.file = obj.file;
     this.default = obj.default;
@@ -20,22 +20,61 @@ export default class Model {
 
   init() {
     this.loader.load(this.file, (res) => {
+      // 3D Model Mesh
       this.mesh = res.scene.children[0];
+
+      // Material Mesh
       this.material = new THREE.MeshBasicMaterial({
         color: "red",
         wireframe: true,
       });
       this.mesh.material = this.material;
 
+      // Geometry Mesh
+      this.geometry = this.mesh.geometry;
+
+      // Particles Material
+      this.particlesMaterial = new THREE.PointsMaterial({
+        color: "red",
+        size: 0.02,
+      });
+
+      // Partciles Geometry
+      const numParticles = 20000;
+      // MeshSurfaceSampler: Utility class for sampling weighted random points on the surface of a mesh.
+      const sampler = new MeshSurfaceSampler(this.mesh).build();
+      this.particlesGeometry = new THREE.BufferGeometry();
+      const particlesPosition = new Float32Array(numParticles * 3);
+
+      for (let i = 0; i < numParticles; i++) {
+        const newPosition = new THREE.Vector3();
+        sampler.sample(newPosition);
+        particlesPosition.set(
+          [newPosition.x, newPosition.y, newPosition.z],
+          i * 3
+        );
+      }
+
+      this.particlesGeometry.setAttribute(
+        "position",
+        new THREE.BufferAttribute(particlesPosition, 3)
+      );
+      console.log(this.particlesGeometry);
+      // Particles
+      this.particles = new THREE.Points(
+        this.particlesGeometry,
+        this.particlesMaterial
+      );
+
       if (this.default) this.add();
     });
   }
 
   add() {
-    this.scene.add(this.mesh);
+    this.scene.add(this.particles);
   }
 
   remove() {
-    this.scene.remove(this.mesh);
+    this.scene.remove(this.particles);
   }
 }
